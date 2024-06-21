fde00ad114d0ba6fb00d4a46d3d1f1dcb4e670a2
jessica.gloor <jessicagloor@hotmail.com>
2024-06-20 22:17:11 -0300

    inicia projeto

---
+++ src/main/java/com/gestao_trabalho/gestao_trabalho/GestaoTrabalhoApplication.java
@@ -0,0 +1,13 @@
+package com.gestao_trabalho.gestao_trabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoTrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoTrabalhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestao_trabalho/gestao_trabalho/GestaoTrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao_trabalho.gestao_trabalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoTrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
